2a4f33f38597939b2e8c2a619ce85ca7f2d6ad2a
unknown <carloschinchillamoreno@gmail.com>
2023-11-14 09:59:36 +0100

    ADD dotenv config at index

--- queries/authors.queries.js
+++ queries/authors.queries.js
@@ -10,8 +10,7 @@ const queries = {
 
     createAuthor:
     `INSERT INTO authors(name,surname,email,image) 
-    VALUES
-    ($1,$2,$3,$4);`,
+    VALUES ($1,$2,$3,$4);`,
 
     updateAuthor:
     `UPDATE authors
